a6f7a8cb3aacbe26e2dab7dab8abef885aba5301
DyFan124 <dyf2208200436@126.com>
2025-10-14 18:59:20 +0800

    改为五局三胜制

--- finger-guessing.c
+++ finger-guessing.c
@@ -8,6 +8,7 @@ int main(){
     SetConsoleOutputCP(65001);
 
     int player_choice,computer_choice;
+    int player_win=0,computer_win=0;
 
     srand(time(NULL));//设置种子保证随机数随机
 
@@ -15,6 +16,17 @@ int main(){
     printf("输入1=石头\t输入2=剪刀\t输入3等于布\n");
 
     while(1){
+        if(player_win==3){
+            printf("你赢了！！\n");
+            break;
+        }
+        if(computer_win==3){
+            printf("你输了！！\n");
+            break;
+        }
+
+        printf("你的胜场:%d\n计算机的胜场:%d\n",player_win,computer_win);
+        
         printf("请出拳:");
         scanf("%d",&player_choice);
 
@@ -41,11 +53,13 @@ int main(){
         if(player_choice==computer_choice)
         printf("平局，再来一次\n");
         else if(player_choice==computer_choice-1||player_choice==computer_choice+2){
-        printf("你赢了！\n");
-        break;    
+        printf("你胜场+1\n");
+        player_win++;
         }
-        else
-        printf("你输了，再来一次\n");
+        else{
+        printf("计算机胜场+1\n");
+        computer_win++;
+        }
     }
     return 0;
 }
